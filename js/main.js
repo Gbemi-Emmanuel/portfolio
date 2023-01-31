2ac9381c35ed5648179ec4b8104ad9771df5f024
Emmanuel Maxify <gbemiemmanuel555@gmail.com>
2023-01-31 01:23:41 +0100

    files updated

--- js/main.js
+++ js/main.js
@@ -214,12 +214,13 @@
    */
 
   var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
+    slidesPerView: 1.5,
     spaceBetween: 30,
     slidesPerGroup: 4,
     freeMode: true,
     loop: true,
     loopFillGroupWithBlank: false,
+    
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -228,6 +229,23 @@
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      770: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    }
     });
 
   /*
